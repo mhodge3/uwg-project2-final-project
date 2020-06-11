6512662430971555b4bce22f589519e5102b433e
Matthew Hodge <mhodge3@my.westga.edu>
2020-06-11 15:30:10 -0700

    New experimental branch created from Latest Eclipse
    JavaFX setup and example program loads form from a fxml file in the view
    package

--- scr/main/java/storyMapper/Main.java
+++ scr/main/java/storyMapper/Main.java
@@ -1,6 +1,13 @@
 package storyMapper;
 
+import java.io.IOException;
+
 import DAL.MySQLAccess;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * Entry point to the RPG Story Mapper program
@@ -9,12 +16,23 @@ import DAL.MySQLAccess;
  * @version 6.10.2020
  */
 
-public class Main {
+public class Main extends Application {
 
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello World");
         MySQLAccess dao = new MySQLAccess();
         dao.readDataBase();
+        launch(args);
 	}
+    
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+    	Parent root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
+    	Scene scene = new Scene(root, 300, 275);
+        
+    	primaryStage.setTitle("FXML Welcome");
+    	primaryStage.setScene(scene);
+    	primaryStage.show();
+    }
 
 }
