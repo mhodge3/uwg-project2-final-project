463f059d58237eb83478825ea29fa68a954bffb1
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-13 21:58:21 -0400

    Created variables, getters, and setters for the Items model class.

--- scr/main/java/model/Items.java
+++ scr/main/java/model/Items.java
@@ -9,5 +9,110 @@ package model;
  *
  */
 public class Items {
+	private int itemId;
+	private String itemName;
+	private String itemDescription;
+	private int itemType;
+	private int isQuestItem;
+	private int isImplicitItem;
+	
+	/**
+	 * Gets the Item Id
+	 * @return itemdID
+	 */
+	public int GetItemId() {
+		return itemId;
+	}
+	
+	/**
+	 * Sets the Item Id
+	 * @param setItemID
+	 */
+	
+	public void SetItemId(int setItemID) {
+		this.itemId = setItemID;
+	}
+	
+	/**
+	 * Gets the Item Name
+	 * @return itemName
+	 */
+	
+	public String GetItemName() {
+		return itemName;
+	}
+	
+	/**
+	 * Sets the Item Name
+	 * @param setItemName
+	 */
+	public void SetItemName(String setItemName) {
+		this.itemName = setItemName;
+	}
+	
+	/**
+	 * Gets the Item Description
+	 * @return itemDescription
+	 */
+	public String GetItemDescription() {
+		return itemDescription;
+	}
+	
+	/**
+	 * Sets the Item Description
+	 * @param setItemDescription
+	 */
+	public void SetItemDescription(String setItemDescription) {
+		this.itemDescription = setItemDescription;
+	}
+	
+	/**
+	 * Gets the Item Type
+	 * @return itemType
+	 */
+	public int GetItemType() {
+		return itemType;
+	}
+	
+	/**
+	 * Sets the Item Type
+	 * @param setItemType
+	 */
+	public void SetItemType(int setItemType) {
+		this.itemType = setItemType;
+	}
+	
+	/**
+	 * Gets the Is Quest Item
+	 * @return isQuestItem
+	 */
+	public int GetIsQuestItem() {
+		return isQuestItem;
+	}
+	
+	/**
+	 * Sets the IsQuestItem
+	 * @param setIsQuestItem
+	 */
+	public void SetIsQuestItem(int setIsQuestItem) {
+		this.isQuestItem = setIsQuestItem;
+	}
+	
+	/**
+	 * Gets the isImplicitItem
+	 * @return isImplicitItem
+	 */
+	public int GetIsImplicitItem() {
+		return isImplicitItem;
+	}
+	
+	/**
+	 * Sets the IsImplicitItem
+	 * @param setIsImplicitItem
+	 */
+	public void SetIsImplicitItem(int setIsImplicitItem) {
+		this.isImplicitItem = setIsImplicitItem;
+	}
+	
 
 }
