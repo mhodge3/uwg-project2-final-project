4f4a599cbb569ae7577ebbc61f779d4f5713e86e
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-15 22:03:50 -0400

    Created testGetNpcIDShouldBe1234() method.

---
+++ scr/main/java/testing/CharacterTests.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package testing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import model.Character;
+
+/**
+ * @author Ashley Palmer
+ * @date 6/15/2020
+ *
+ */
+public class CharacterTests {
+	
+	/**
+	 * Tests the GetNpcId and SetNpcId Methods
+	 * 
+	 */
+	@Test
+	public void testGetNpcIDShouldBe1234() {
+		Character testCharacter = new Character();
+		testCharacter.SetNpcId(1234);
+		assertEquals(1234, testCharacter.GetNpcId());
+	}
+	
+	
+
+}
