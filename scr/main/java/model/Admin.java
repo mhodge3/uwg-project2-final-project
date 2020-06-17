5d62a5fc491a40fe439f75b12844d87a680a2aec
Perry Iler <piler1@my.westga.edu>
2020-06-17 02:06:50 -0400

    Added javaFX components and overloaded constructor

--- scr/main/java/model/Admin.java
+++ scr/main/java/model/Admin.java
@@ -3,45 +3,67 @@
  */
 package model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 /**
  * @author Ashley Palmer
  * @date 6/11/2020
  *
  */
 public class Admin extends Player {
-	private int adminId;
-	private int playerID;
+	private IntegerProperty adminId;
+	private IntegerProperty playerId;
+	
+	/**
+	 * Default Player constructor. Not currently used, but exists for tests
+	 */
+	public Admin() {}
+	
+	public Admin(int adminId, int playerId) {
+		this.adminId = new SimpleIntegerProperty(adminId);
+		this.playerId = new SimpleIntegerProperty(playerId);
+	}
 	
 	/**
 	 * Gets the adminID
 	 * @return adminId
 	 */
 	public int GetAdminId() {
-		return adminId;
+		return adminId.get();
 	}
+	
+	/**
+	 * Gets the playerId property
+	 * @return Property for TableView
+	 */
+	public final IntegerProperty adminIdProperty() {
+	   return playerId;
+	}
+	
 	
 	/**
 	 * Sets the AdminId
 	 * @param setAdminId
 	 */
-	public void SetAdminID(int setAdminId) {
-		this.adminId = setAdminId;
+	public void SetAdminID(int adminId) {
+		this.adminId = new SimpleIntegerProperty(adminId);
 	}
 	
 	/**
-	 * Gets the playerId
-	 * return playerId
+	 * Gets playerId
+	 * @return playerId
 	 */
 	public int GetPlayerId() {
-		return playerID;
+		return playerId.get();
 	}
 	
 	/**
 	 * Sets the playerId
-	 * @param setPlayerId
+	 * @param playerId
 	 */
-	public void SetPlayerID(int setPlayerId) {
-		this.playerID = setPlayerId;
+	public void SetPlayerId(int playerId) {
+		this.playerId = new SimpleIntegerProperty(playerId);
 	}
 
 }
