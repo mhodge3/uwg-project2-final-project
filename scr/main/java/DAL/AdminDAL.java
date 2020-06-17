b3260b5888d22290ba32034e3e9dd5aa9dd59f9c
Perry Iler <piler1@my.westga.edu>
2020-06-17 04:18:03 -0400

    Added and tested createAdmin method for AdminDAL

--- scr/main/java/DAL/AdminDAL.java
+++ scr/main/java/DAL/AdminDAL.java
@@ -3,6 +3,7 @@ package DAL;
 import java.sql.*;
 
 import model.Admin;
+import model.Player;
 /**
  * Data access layer for the Admins database table
  * @author Perry Iler
@@ -12,19 +13,55 @@ import model.Admin;
 public class AdminDAL {
 	private MySQLAccess sqlAccess;
 	private Connection conn;
+	private String dataBase;
 	
-	public AdminDAL() {
-		this.sqlAccess = new MySQLAccess();
+	public AdminDAL(MySQLAccess theDBConnection) {
+		this.sqlAccess = theDBConnection;
+		this.dataBase = this.sqlAccess.GetTheDBName();
 		
 	}
-	
 
-	public Admin getAdmin() throws Exception {
-     
+	public int getAdminID(int playerID) throws Exception {
 		Admin admin = null;
-		
-		
-		return admin;
+        try {
+            this.conn = this.sqlAccess.GetDBConnection();
+            Statement statement = this.conn.createStatement();
+            String query = "SELECT * FROM `" + this.dataBase + "`.`admins` "
+            		+ "WHERE `" + this.sqlAccess.GetTheDBName() + "`.admins.`player_id` = " + playerID + "\";";
+            ResultSet results = statement.executeQuery(query);
+            if (results.next() != false) {
+                admin = new Admin();
+                admin.SetAdminID(Integer.parseInt(results.getString("admin_id")));
+                admin.SetAdminID(Integer.parseInt(results.getString("player_id")));
+            }
+            
+        } catch (Exception e) {
+        }
+        finally {
+        	conn.close();
+        }
+		return admin.GetPlayerId();
     }
+	
+	public boolean CreateAdmin(int playerId) throws SQLException {
+		Boolean success = false;
+		try {
+			this.conn = this.sqlAccess.GetDBConnection();
+			String query = "INSERT INTO " + this.dataBase + ".`admins`" + 
+					"(`player_id`)" + 
+					"VALUES (?)";
+			 PreparedStatement preparedStmt = conn.prepareStatement(query);
+			  preparedStmt.setString (1, String.valueOf(playerId));
+			  			  
+		      preparedStmt.execute();
+		      success = true;
+		} catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return success;
+	}
 
 }
---
+++ scr/test/java/DAL/testAdminDAL.java
@@ -0,0 +1,28 @@
+package DAL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.Player;
+
+class testAdminDAL {
+	Player thePlayer = new Player();
+	MySQLAccess access = new MySQLAccess();
+	AdminDAL adminDal = new AdminDAL(access);
+	PlayerDAL playerDal = new PlayerDAL(access);
+	
+	@Test
+	public void createAdmin() throws Exception {
+		this.thePlayer = this.playerDal.GetPlayer("player", "test1234");
+		assertTrue(this.adminDal.CreateAdmin(thePlayer.GetPlayerId()));
+	}
+	
+	@Test
+	public void getTheAdminId() throws Exception {
+		this.thePlayer = this.playerDal.GetPlayer("admin", "test1234");
+		assertEquals(this.adminDal.getAdminID(thePlayer.GetPlayerId()), 1);
+	}
+		
+
+}
