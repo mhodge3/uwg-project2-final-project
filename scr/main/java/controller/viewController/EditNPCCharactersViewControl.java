2eba7651b521a52aadb6f9bca69634f22981775e
Matthew Hodge <mhodge3@my.westga.edu>
2020-06-19 07:59:04 -0700

    NPC Character edits now save.
    NPC Characters are functionally complete for Iteration 1.

--- scr/main/java/controller/viewController/EditNPCCharactersViewControl.java
+++ scr/main/java/controller/viewController/EditNPCCharactersViewControl.java
@@ -3,10 +3,11 @@ package controller.viewController;
 import java.sql.SQLException;
 
 import controller.logicController.EditNPCCharactersControl;
-import controller.logicController.EditPlayersAndAdminsControl;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.NpcCharacter;
 
 public class EditNPCCharactersViewControl {
@@ -46,6 +47,25 @@ public class EditNPCCharactersViewControl {
     
 	@FXML
 	private void handleNPCEditSaveButton() throws SQLException {
-		//TODO save logic
+		String npcCreationError = null;
+		try {
+			npcCreationError = theEditNPCCharactersControl.UpdateNpc(theEditNPCCharactersControl.GetSelectedNPC().GetNpcId(), Integer.parseInt(editNPCType.getText()), editNPCNameTextBox.getText(), editNPCDescriptionTextBox.getText());
+		} catch (Exception e) {
+			npcCreationError = e.getMessage();
+		}
+		if (npcCreationError != null) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Error Dialog");
+			alert.setHeaderText("Error Editing the NPC");
+			alert.setContentText(npcCreationError);
+			alert.showAndWait();
+			return;
+		}
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("NPC Edit");
+		alert.setHeaderText("NPC Edit Status");
+		alert.setContentText("The NPC was successfully modified");
+		alert.showAndWait();
+		theMainDashboardViewControl.SetMainDashboardStage("manageNPCCharacters");
 	}
 }
