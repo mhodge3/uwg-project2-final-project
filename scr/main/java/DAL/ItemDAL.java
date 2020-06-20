55463c26a7daeace6f7b0ae73d575b076ca4fcea
Perry Iler <piler1@my.westga.edu>
2020-06-19 21:24:52 -0400

    Added the CRUD methods to the ItemDAL

--- scr/main/java/DAL/ItemDAL.java
+++ scr/main/java/DAL/ItemDAL.java
@@ -1,10 +1,14 @@
 package DAL;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import model.Item;
+import model.NpcCharacter;
 
 /**
  * Data Access Layer for Items
@@ -29,7 +33,154 @@ public class ItemDAL {
 	 */
 	public ArrayList<Item> GetItems() throws SQLException {
 		ArrayList<Item> theItemArrayList = new ArrayList<Item>();
+		
+		try {
+            this.conn = this.sqlAccess.GetDBConnection();
+            Statement statement = this.conn.createStatement();
+            String query = "SELECT  item_id," + 
+            		"    item_name, " + 
+            		"    item_description, " + 
+            		"    item_type, " + 
+            		"    is_quest_item, " + 
+            		"    is_implicit_item " +
+            	"FROM " + this.sqlAccess.GetTheDBName() + ".items ";
+            ResultSet results = statement.executeQuery(query);
+            while (results.next() != false) {
+            	Item item = new Item();
+                item.SetItemId(Integer.parseInt(results.getString("item_id")));
+                item.SetItemName(results.getString("character_item_name"));
+                item.SetItemDescription(results.getString("character_item_description"));
+                item.SetItemType(Integer.parseInt(results.getString("item_type")));
+                item.SetIsQuestItem(Integer.parseInt(results.getString("is_quest_item")));
+                item.SetIsImplicitItem(Integer.parseInt(results.getString("is_implicit_item")));
+                theItemArrayList.add(item);
+            }
+        } catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
 		return theItemArrayList; 
 	}
+	
+	/**
+	 * Get Item from the database Items table
+	 * @return The Item
+	 * @throws SQLException
+	 */
+	public Item GetItem(int itemId) throws SQLException {
+		Item item = null;
+		
+		try {
+            this.conn = this.sqlAccess.GetDBConnection();
+            Statement statement = this.conn.createStatement();
+            String query = "SELECT  item_id," + 
+            		"item_name, " + 
+            		"item_description, " + 
+            		"item_type, " + 
+            		"is_quest_item, " + 
+            		"is_implicit_item " +
+            	"FROM " + this.sqlAccess.GetTheDBName() + ".items " +
+            		"WHERE item_id = \"" + itemId + "\"";
+            ResultSet results = statement.executeQuery(query);
+            if (results.next() != false) {
+            	item = new Item();
+                item.SetItemId(Integer.parseInt(results.getString("item_id")));
+                item.SetItemName(results.getString("item_name"));
+                item.SetItemDescription(results.getString("item_description"));
+                item.SetItemType(Integer.parseInt(results.getString("item_type")));
+                item.SetIsQuestItem(Integer.parseInt(results.getString("is_quest_item")));
+                item.SetIsImplicitItem(Integer.parseInt(results.getString("is_implicit_item")));
+            }
+        } catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return item; 
+	}
+	
+	public Boolean CreateItem(String itemName, String itemDescription, int itemType, int isQuestItem, int isImplicitItem) throws SQLException {
+		Boolean success = false;
+		try {
+			this.conn = this.sqlAccess.GetDBConnection();
+			String query = "INSERT INTO " + this.sqlAccess.GetTheDBName() + ".items" + 
+					"item_name, " + 
+            		"item_description, " + 
+            		"item_type, " + 
+            		"is_quest_item, " + 
+            		"is_implicit_item " +
+					"VALUES (?, ?, ?, ?, ?)";
+			 PreparedStatement preparedStmt = conn.prepareStatement(query);
+			  preparedStmt.setString (1, itemName);
+			  preparedStmt.setString (2, itemDescription);
+			  preparedStmt.setString (3, String.valueOf(itemType));
+			  preparedStmt.setString (4, String.valueOf(isQuestItem));
+			  preparedStmt.setString (5, String.valueOf(isImplicitItem));
+			  
+		      preparedStmt.execute();
+		      success = true;
+		} catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return success;
+	}
+	
+	public Boolean UpdateNpc(Item oldItem, Item updatedItem) throws SQLException {
+		Boolean success = false;
+		try {
+			this.conn = this.sqlAccess.GetDBConnection();
+			String query = "UPDATE " + this.sqlAccess.GetTheDBName() + ".items " + 
+					"SET " +
+					"item_name = ?, " + 
+            		"item_description = ?, " + 
+            		"item_type = ?, " + 
+            		"is_quest_item = ?, " + 
+            		"is_implicit_item = ? " +
+            	    "WHERE item_id = ?";
+		
+			 PreparedStatement preparedStmt = conn.prepareStatement(query);
+			  preparedStmt.setString (1, updatedItem.GetItemName());
+			  preparedStmt.setString (2, updatedItem.GetItemDescription());
+			  preparedStmt.setString (3, String.valueOf(updatedItem.GetItemType()));
+			  preparedStmt.setString (4, String.valueOf(updatedItem.GetIsImplicitItem()));
+			  preparedStmt.setString (5, String.valueOf(updatedItem.GetIsImplicitItem()));
+			  preparedStmt.setString (6, String.valueOf(oldItem.GetItemId()));
+		      preparedStmt.execute();
+		      success = true;
+		} catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return success;
+	}
+	
+	public boolean DeleteItem(Item item) throws SQLException {
+		Boolean success = false;
+		try {
+			this.conn = this.sqlAccess.GetDBConnection();
+			String query = "DELETE FROM " + this.sqlAccess.GetTheDBName() + ".items " + 
+					"WHERE item_id = ?";
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			preparedStmt.setString (1, String.valueOf(item.GetItemId()));
+			
+			preparedStmt.execute();
+		      success = true;
+		} catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return success;
+	}
+	
 
 }
