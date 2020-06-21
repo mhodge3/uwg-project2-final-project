85dcd60dc3488d81a5b99fe4b463c2f7e11376a3
Perry Iler <piler1@my.westga.edu>
2020-06-21 15:27:35 -0400

    Tested overloaded constructor and get/set methods for isActive

--- scr/test/java/model/AdminTests.java
+++ scr/test/java/model/AdminTests.java
@@ -16,6 +16,17 @@ import model.Admin;
 public class AdminTests {
 	
 	/**
+	 * Tests Admin model's overloaded constructor
+	 */
+	@Test
+	public void testAdminOverloadedContructor() {
+		Admin testAdmin = new Admin(1, 2, 3);
+		assertEquals(1, testAdmin.GetAdminId());
+		assertEquals(2, testAdmin.GetPlayerId());
+		assertEquals(3, testAdmin.GetIsActive());
+	}
+	
+	/**
 	 * Tests the GetAdminID and SetAdminID Methods
 	 * 
 	 */
@@ -37,5 +48,15 @@ public class AdminTests {
 		assertEquals(1234, testAdmin.GetPlayerId());
 	}
 	
+	/**
+	 * Tests the GetIsActive and SetIsActive Methods
+	 * 
+	 */
+	@Test
+	public void testGetIsActiveShouldBe1234() {
+		Admin testAdmin = new Admin();
+		testAdmin.SetIsActive(1);;
+		assertEquals(1, testAdmin.GetIsActive());
+	}
 
 }
