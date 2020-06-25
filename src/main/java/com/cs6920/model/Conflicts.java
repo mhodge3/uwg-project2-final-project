d9bdbd774fbee7cd90b0a27c43c34b592ad46669
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-25 14:48:42 -0400

    Created Conflicts class.

---
+++ src/main/java/com/cs6920/model/Conflicts.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.cs6920.model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * @author Ashley Palmer
+ * @date 6/25/2020
+ *
+ */
+public class Conflicts {
+	
+	private IntegerProperty conflictId;
+	private IntegerProperty conflictTemplate;
+	private StringProperty conflictName;
+	private StringProperty conflictDescription;
+	
+	
+
+}
