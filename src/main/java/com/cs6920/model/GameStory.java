bc445f7ca25f62641a3dbb2ba5613bf3dd6b07e4
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-25 13:31:32 -0400

    Palmmer first commit this branch.

---
+++ src/main/java/com/cs6920/model/GameStory.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.cs6920.model;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ * @author Ashley Palmer
+ * @date 6/25/2020
+ *
+ */
+public class GameStory {
+	
+	
+	private StringProperty gameStoryName;
+	private StringProperty gameStorySummary;
+
+}
