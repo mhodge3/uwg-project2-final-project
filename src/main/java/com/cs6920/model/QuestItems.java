de5d61deaa39748be800159fdb183d503527caea
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-25 13:54:20 -0400

    Created QuestItems class with variables.

---
+++ src/main/java/com/cs6920/model/QuestItems.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.cs6920.model;
+
+import javafx.beans.property.IntegerProperty;
+
+/**
+ * @author Ashley Palmer
+ * @date 6/25/2020
+ *
+ */
+public class QuestItems {
+	
+	private IntegerProperty questId;
+	private IntegerProperty itemId;
+	private IntegerProperty itemQuantity;
+
+}
