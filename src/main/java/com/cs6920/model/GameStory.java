0816934dabe437fd4688e7f5ae03699d9748d95f
Lewis Baumstark <lewisb@westga.edu>
2020-06-25 13:34:06 -0400

    Created methods for  getting, final, and setting gamestory name.

--- src/main/java/com/cs6920/model/GameStory.java
+++ src/main/java/com/cs6920/model/GameStory.java
@@ -3,6 +3,7 @@
  */
 package com.cs6920.model;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
@@ -15,5 +16,29 @@ public class GameStory {
 	
 	private StringProperty gameStoryName;
 	private StringProperty gameStorySummary;
+	
+	/**
+	 * Gets the gameStorn Name
+	 * @return gameStoryName
+	 */
+	public String GetPlayerName() {
+		return gameStoryName.get();
+	}
+	
+	/**
+	 * Gets the game story name property
+	 * @return Property for TableView
+	 */
+	public final StringProperty playerNameProperty() {
+	   return gameStoryName;
+	}
+	
+	/**
+	 * Sets the gameStoryName
+	 * @param gameStoryName
+	 */
+	public void SetPlayerName(String gameStoryName) {
+		this.gameStoryName = new SimpleStringProperty(gameStoryName);
+	}
 
 }
