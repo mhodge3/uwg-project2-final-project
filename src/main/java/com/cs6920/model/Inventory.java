3486e57e15c171668faba35f58dc4befe5298975
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-25 22:48:43 -0400

    Created getter, final, and setter for itemId.

--- src/main/java/com/cs6920/model/Inventory.java
+++ src/main/java/com/cs6920/model/Inventory.java
@@ -3,6 +3,9 @@
  */
 package com.cs6920.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 /**
  * @author Ashley Palmer
  * @date 6/25/2020
@@ -10,6 +13,34 @@ package com.cs6920.model;
  */
 
 public class Inventory {
+	
+	private IntegerProperty itemId;
+	private IntegerProperty characterId;
+	
+	/**
+	 * Gets the Item Id
+	 * @return itemdID
+	 */
+	public int GetItemId() {
+		return itemId.get();
+	}
+	
+	/**
+	 * Sets the Item Id
+	 * @param setItemID
+	 */
+	
+	public void SetItemId(int setItemID) {
+		this.itemId = new SimpleIntegerProperty(setItemID);
+	}
+	
+	/**
+	 * Gets the ItemID property
+	 * @return Property for TableView
+	 */
+	public final IntegerProperty itemIdProperty() {
+	   return itemId;
+	}
 	
 
 }
