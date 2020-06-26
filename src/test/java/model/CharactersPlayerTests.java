cfcf25e6e4b9e06f51303279f875d2e7f9e2397a
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-26 11:49:06 -0400

    Created testGetCharacteIDShouldBe1234() method.

--- src/test/java/model/CharactersPlayerTests.java
+++ src/test/java/model/CharactersPlayerTests.java
@@ -3,6 +3,12 @@
  */
 package model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.cs6920.model.*;
+
 /**
  * @author Ashley Palmer
  * @date 6/26/2020
@@ -11,6 +17,15 @@ package model;
 
 public class CharactersPlayerTests {
 	
-	
+	/**
+	 * Tests the GetCharacterD and SetCharacterID Methods
+	 * 
+	 */
+	@Test
+	public void testGetCharacteIDShouldBe1234() {
+		CharactersPlayer testItem = new CharactersPlayer();
+		testItem.SetCharacterId(1234);
+		assertEquals(1234, testItem.GetCharacterId());
+	}
 
 }
