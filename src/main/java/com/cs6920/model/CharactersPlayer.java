a6dcca2681689c6fb35459958318847daa3d2ea0
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-25 22:58:46 -0400

    Created CharactersPlayer class.

---
+++ src/main/java/com/cs6920/model/CharactersPlayer.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.cs6920.model;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * @author Ashley Palmer
+ * @date 6/25/2020
+ *
+ */
+public class CharactersPlayer {
+	private IntegerProperty characterId;
+	private IntegerProperty characterPlayerId;
+	private StringProperty characterName;
+	private IntegerProperty characterType;
+	private IntegerProperty characterFaction;
+	private DoubleProperty npcPosX;
+	private DoubleProperty npcPosY;
+	private DoubleProperty npcPosZ;
+	
+	
+
+}
