d0d05aeaab20439514cd748be1a7040a04e1137a
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-26 10:23:48 -0400

    Created testGetQuestIDShouldBe1234() method.

---
+++ src/test/java/model/QuestsTests.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import com.cs6920.model.*;
+
+/**
+ * @author Ashley Palmer
+ * @date 6/26/2020
+ *
+ */
+public class QuestsTests {
+	
+	/**
+	 * Tests the GetQuestID and SetQuestID Methods
+	 * 
+	 */
+	@Test
+	public void testGetQuestIDShouldBe1234() {
+		Quests testItem = new Quests();
+		testItem.SetQuestId(1234);
+		assertEquals(1234, testItem.GetQuestId());
+	}
+	
+	
+
+}
