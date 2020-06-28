9987245bb9085b72d2ee1f023108bf9fb119f779
Ashley Palmer <apalmer4@my.westga.edu>
2020-06-28 13:06:49 -0400

    Created CreateGameStoryView class.

--- src/main/java/com/cs6920/control/view_control/EditGameStoryViewControl.java
+++ src/main/java/com/cs6920/control/view_control/EditGameStoryViewControl.java
@@ -13,7 +13,6 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.CheckBox;
 import com.cs6920.model.Item;
 
 /**
