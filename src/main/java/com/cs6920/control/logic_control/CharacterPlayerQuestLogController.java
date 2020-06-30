89f70177e708718804f3da44a5781b7f19ec9d1f
Perry Iler <piler1@my.westga.edu>
2020-06-30 12:08:01 -0400

    Created a controller for the CharactersPlayerDAL

--- src/main/java/com/cs6920/control/logic_control/CharacterPlayerQuestLogController.java
+++ src/main/java/com/cs6920/control/logic_control/CharacterPlayerQuestLogController.java
@@ -1,5 +1,113 @@
 package com.cs6920.control.logic_control;
 
-public class CharacterPlayerQuestLogController {
+import java.sql.SQLException;
 
+import com.cs6920.DAL.CharacterPlayerQuestLogDAL;
+import com.cs6920.DAL.MySQLAccess;
+import com.cs6920.model.CharacterPlayerQuestLog;
+
+/**
+* Controller class that retrieves data from the CharacterPlayerQuestLogDAL
+* @author Perry Iler
+* @date 6/29/2020
+*
+*/
+public class CharacterPlayerQuestLogController {
+	private CharacterPlayerQuestLogDAL dal;
+	
+	/**
+	 * Constructor for the CharacterPlayerQuestLogController
+	 * @param theDBConnection
+	 */
+	public CharacterPlayerQuestLogController(MySQLAccess theDBConnection) {
+		this.dal = new CharacterPlayerQuestLogDAL(theDBConnection);
+	}
+	
+	/**
+	 * Calls the CreateCharacterPlayerQuestLog method from the DAL
+	 * @param characterId
+	 * @param questId
+	 * @param questStatus
+	 * @return Error message
+	 * @throws SQLException
+	 */
+	public String CreateCharacterPlayerQuestLog(int characterId, int questId, int questStatus) {
+		
+		try {
+			if(this.dal.CreateCharacterPlayerQuestLog(characterId, questId, questStatus)) {
+				return null;
+			}
+			else {
+				return "There was a problem creating the quest log";
+			}
+		} catch (SQLException e) {
+			return "There was a problem creating the quest log";
+		}
+	}
+	
+	/**
+	 * Calls the GetCharacterPlayerQuestLogByQuestID method from the DAL
+	 * 
+	 * @param QuestId
+	 * @return The characterPlayerQuestLog looked up
+	 */
+	public CharacterPlayerQuestLog GetCharacterPlayerQuestLogByQuestId(int QuestId) {
+		try {
+			return this.dal.GetCharacterPlayerQuestLogByQuestId(QuestId);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+    }
+
+	/**
+	 * Calls the GetCharacterPlayerQuestLogByQuestID method from the DAL
+	 * @param characterID
+	 * @return The characterPlayerQuestLog looked up
+	 */
+	public CharacterPlayerQuestLog GetCharacterPlayerQuestLogByCharacterId(int characterID) {
+		try {
+			return this.dal.GetCharacterPlayerQuestLogByCharacterId(characterID);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+    }
+	
+	/**
+	 * Calls the UpdateCharacterPlayerQuestLogStatus method from the DAL
+	 * @param log
+	 * @param updatedStatus
+	 * @return Error message
+	 */
+	public String UpdateCharacterPlayerQuestLogStatus(CharacterPlayerQuestLog log, int updatedStatus) {
+		try {
+			if(this.dal.UpdateCharacterPlayerQuestLogStatus(log, updatedStatus)) {
+				return null;
+			}
+			else {
+				return "There was a problem updating the quest log";
+			}
+		} catch (Exception e) {
+			return "There was a problem updating the quest log";
+        }
+	}
+	
+	/**
+	 * Calls the DeleteCharacterPlayerQuestLog method from the DAL
+	 * @param log
+	 * @return Error message
+	 */
+	public String DeleteCharacterPlayerQuestLog(CharacterPlayerQuestLog log) {
+		try {
+			if(this.dal.DeleteCharacterPlayerQuestLog(log)) {
+				return null;
+			}
+			else {
+				return "There was a problem deleting the quest log";
+			}
+		} catch (Exception e) {
+			return "There was a problem deleting the quest log";
+        }
+	}
 }
---
+++ src/main/java/com/cs6920/control/logic_control/CharactersPlayerController.java
@@ -0,0 +1,140 @@
+package com.cs6920.control.logic_control;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.cs6920.DAL.CharactersPlayerDAL;
+import com.cs6920.DAL.MySQLAccess;
+import com.cs6920.model.CharactersPlayer;
+
+/**
+* Controller class that retrieves data from the CharactersPlayerDAL
+* @author Perry Iler
+* @date 6/29/2020
+*
+*/
+public class CharactersPlayerController {
+
+	private CharactersPlayerDAL dal;
+	
+	/**
+	 * Constructor for the CharactersPlayerController
+	 * @param theDBConnection
+	 */
+	public CharactersPlayerController(MySQLAccess theDBConnection) {
+		this.dal = new CharactersPlayerDAL(theDBConnection);
+	}
+	
+	/**
+	 * Creates a characterPlayer entry in the database CharacterPlayer table
+	 * @param characterId
+	 * @param characterPlayerId
+	 * @param characterName
+	 * @param characterType
+	 * @param characterFaction
+	 * @param characterPosX
+	 * @param characterPosY
+	 * @param characterPosZ
+	 * @return Error message
+	 */
+	public String CreateCharacterPlayer(int characterPlayerId, String characterName, int characterType, int characterFaction, double characterPosX, double characterPosY, double characterPosZ) {
+		try {
+			if(String.valueOf(characterPlayerId) == null || characterPlayerId <= 0) {
+				return "The player ID cannot be empty or less than or equal to zero";
+			}
+			else if (characterName == null || characterName.trim().length() == 0) {
+				return "The User characterName cannot be empty";
+			}
+			else if(String.valueOf(characterType) == null || characterType <= 0) {
+				return "The type cannot be empty or less than or equal to zero";
+			}
+			else if(String.valueOf(characterFaction) == null || characterFaction <= 0) {
+				return "The fraction cannot be empty or less than or equal to zero";
+			}
+			else if(String.valueOf(characterPosX) == null) {
+				return "The posX cannot be empty.";
+			}
+			else if(String.valueOf(characterPosY) == null) {
+				return "The posY cannot be empty";
+			}
+			else if(String.valueOf(characterPosX) == null) {
+				return "The posY cannot be empty";
+			}
+			
+			if (this.dal.CreateCharacterPlayer(characterPlayerId, characterName, characterType, characterFaction, characterPosX, characterPosY, characterPosZ)) {
+				return null;
+			}
+			else {
+				return "There was a problem creating the characterPlayer";
+			}
+			
+		} catch (Exception e) {
+			return "There was a problem creating the characterPlayer";
+        }
+       
+	}
+	
+	/**
+	 * 
+	 * @param Character ID
+	 * @return The CharactersPlayer looked up
+	 * @throws SQLException
+	 */
+	public CharactersPlayer GetCharactersPlayerByID(int characterId) throws SQLException {
+    	return this.dal.GetCharactersPlayerByID(characterId);           
+        
+    }
+	
+	/**
+	 * Calls GetCharactersPlayerByID method from the DAL
+	 * @param Character name
+	 * @return The CharactersPlayer looked up
+	 * @throws SQLException
+	 */
+	public CharactersPlayer GetCharactersPlayerByName(String CharacterName) throws SQLException {
+    	return this.GetCharactersPlayerByName(CharacterName);
+    }
+	
+	/**
+	 * Calls updateCharactersPlayer method from the DAL
+	 * @param oldCharacter
+	 * @param updatedCharacter
+	 * @return Error message
+	 */
+	public String updateCharacterPlayer(CharactersPlayer oldCharacter, CharactersPlayer updatedCharacter) {
+		try {
+			if (this.dal.UpdateCharacterPlayer(oldCharacter, updatedCharacter)) {
+				return null;
+			}
+			else {
+				return "There was a problem updating the characterPlayer";
+			}
+			
+		} catch (Exception e) {
+			return "There was a problem updating the characterPlayer";
+        }
+	}
+	
+	/**
+	 * Calls updateCharactersPlayer method from the DAL
+	 * @param oldCharacter
+	 * @param updatedCharacter
+	 * @return Error message
+	 */
+	public String DeleteCharacterPlayer(CharactersPlayer character) {
+		try {
+			if (this.dal.DeleteCharacterPlayer(character)) {
+				return null;
+			}
+			else {
+				return "There was a problem deleting the characterPlayer";
+			}
+			
+		} catch (Exception e) {
+			return "There was a problem deleting the characterPlayer";
+        }
+	}
+	
+}
