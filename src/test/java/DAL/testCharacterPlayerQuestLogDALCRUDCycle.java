2a30d2abe48108fdf877cca4b8f50c308c3661e6
Perry Iler <piler1@my.westga.edu>
2020-06-30 07:00:13 -0400

    Tested CRUD methods of CharacterPlayerQuestLogDAL

---
+++ src/test/java/DAL/testCharacterPlayerQuestLogDALCRUDCycle.java
@@ -0,0 +1,72 @@
+package DAL;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import com.cs6920.DAL.CharacterPlayerQuestLogDAL;
+import com.cs6920.DAL.MySQLAccess;
+import com.cs6920.model.CharacterPlayerQuestLog;
+
+class testCharacterPlayerQuestLogDALCRUDCycle {
+
+	MySQLAccess access = new MySQLAccess();
+	CharacterPlayerQuestLogDAL dal = new CharacterPlayerQuestLogDAL(access);
+	CharacterPlayerQuestLog log = new CharacterPlayerQuestLog();
+
+	@Test
+	@Order(1)
+	public void createACharactersPlayerQuestLogInDB() throws Exception {
+		assertTrue(this.dal.CreateCharacterPlayerQuestLog(1, 1, 1));
+		
+	}
+	
+	/**
+	 * Testing foreign key constraints
+	 * @throws Exception 
+	 */
+	@Test
+	@Order(2)
+	public void createANonPlayerCharactersLogInDB() throws Exception {
+		assertFalse(this.dal.CreateCharacterPlayerQuestLog(1, 10000, 1));
+		
+	}
+	
+	@Test
+	@Order(3)
+	public void getQuestLogCharacterIDFromDB() throws Exception {
+		this.log = this.dal.GetCharacterPlayerQuestLogByCharacterId(1);
+		assertEquals(this.log.GetCharacterId(), 1);
+		assertEquals(this.log.GetQuestStatus(), 1);
+	}
+	
+	/*@Test
+	@Order(4)
+	public void getQuestLogQuestIDFromDB() throws Exception {
+		this.log = this.dal.GetCharacterPlayerQuestLogByQuestID(1);
+		assertEquals(this.log.GetQuestId(), 1);
+		assertEquals(this.log.GetCharacterId(), 1);
+		assertEquals(this.log.GetQuestStatus(), 1);
+	}*/
+	
+	@Test
+	@Order(4)
+	public void updateQuestLogStatusInTheDB() throws Exception {
+		this.log = this.dal.GetCharacterPlayerQuestLogByCharacterId(1);
+		this.dal.UpdateCharacterPlayerQuestLogStatus(log, 0);
+		this.log = this.dal.GetCharacterPlayerQuestLogByCharacterId(1);
+		assertEquals(this.log.GetCharacterId(), 1);
+		assertEquals(this.log.GetQuestStatus(), 0);
+	}
+	
+	
+/*	@Test
+	@Order(6)
+	public void deleteQuestLogStatusInTheDB() throws Exception {
+		this.log = this.dal.GetCharacterPlayerQuestLogByCharacterId(1);
+		this.dal.UpdateCharacterPlayerQuestLogStatus(log, 0);
+		this.log = this.dal.GetCharacterPlayerQuestLogByCharacterId(1);
+		assertEquals(this.log, null);
+	}*/
+}
