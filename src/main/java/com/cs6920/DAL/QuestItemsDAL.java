492a3e1ac04ae690c3879ef9bc563b0aecf81b34
Perry Iler <piler1@my.westga.edu>
2020-07-05 20:33:24 -0400

    Finished the CRUD methods

--- src/main/java/com/cs6920/DAL/QuestItemsDAL.java
+++ src/main/java/com/cs6920/DAL/QuestItemsDAL.java
@@ -60,14 +60,55 @@ public class QuestItemsDAL {
 		return theQuestItemsArrayList; 
 	}
 	
-	public Boolean DeleteQuestItemsByQuestId(int questId) throws SQLException {
+	/**
+	 * Get Quest Items in QuestItems table
+	 * @return The Array List of QuestItems
+	 * @throws SQLException
+	 */
+	public QuestItems GetQuestItem(int questId, int itemId) throws SQLException {
+		QuestItems questItem = null;
+		
+		try {
+            this.conn = this.sqlAccess.GetDBConnection();
+            Statement statement = this.conn.createStatement();
+            String query = "SELECT  quest_id, " + 
+            		"item_id, " + 
+            		"item_quantity, " + 
+            		"item_display_name " + 
+            	"FROM " + this.sqlAccess.GetTheDBName() + ".questitems "
+            			+ "WHERE `quest_id` = \"" + questId + "\" AND item_id = \"" + itemId + "\"";
+            ResultSet results = statement.executeQuery(query);
+            if (results.next() != false) {
+            	questItem = new QuestItems();
+                questItem.SetQuestId(Integer.parseInt(results.getString("quest_id")));
+                questItem.SetItemId(Integer.parseInt(results.getString("item_id")));
+                questItem.SetItemQuantity(Integer.parseInt(results.getString("item_quantity")));
+                questItem.SetItemDisplayName(results.getString("item_display_name"));
+            }
+        } catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return questItem; 
+	}
+	
+	/**
+	 * Deletes an existing quest item entry in the database QuestItems table
+	 * @param questId
+	 * @return True if deleted | False if not deleted
+	 * @throws SQLException
+	 */
+	public Boolean DeleteQuestItemsByQuestId(QuestItems questItem) throws SQLException {
 		Boolean success = false;
 		try {
 			this.conn = this.sqlAccess.GetDBConnection();
 			String query = "DELETE FROM " + this.sqlAccess.GetTheDBName() + ".questitems " + 
-					"WHERE quest_id = ?";
+					"WHERE quest_id = ? AND item_id = ?";
 			PreparedStatement preparedStmt = conn.prepareStatement(query);
-			preparedStmt.setString (1, String.valueOf(questId));
+			preparedStmt.setString(1, String.valueOf(questItem.GetQuestId()));
+			  preparedStmt.setString(2, String.valueOf(questItem.GetItemId()));
 			
 			preparedStmt.execute();
 		      success = true;
@@ -80,6 +121,12 @@ public class QuestItemsDAL {
 		return success;
 	}
 	
+	/**
+	 * Creates an existing quest item entry in the database QuestItems table
+	 * @param questId
+	 * @return True if deleted | False if not deleted
+	 * @throws SQLException
+	 */
 	public Boolean CreateQuestItem(int questId, int itemId, int itemQuantity, String itemDisplayName) throws SQLException {
 		Boolean success = false;
 		try {
@@ -95,6 +142,36 @@ public class QuestItemsDAL {
 			  preparedStmt.setString(2, String.valueOf(itemId));
 			  preparedStmt.setString(3, String.valueOf(itemQuantity));
 			  preparedStmt.setString(4, itemDisplayName);
+		      preparedStmt.execute();
+		      success = true;
+		} catch (Exception e) {
+        	System.err.println(e.getMessage());
+        }
+        finally {
+        	conn.close();
+        }
+		return success;
+	}
+	
+	/**
+	 * Updates an existing quest item quantity in the database QuestItems table
+	 * @param questItem
+	 * @param itemQuantity
+	 * @return True if updated | False if not updated
+	 * @throws SQLException
+	 */
+	public Boolean UpdateQuestItemQuantity(QuestItems questItem, int itemQuantity) throws SQLException {
+		Boolean success = false;
+		try {
+			this.conn = this.sqlAccess.GetDBConnection();
+			String query = "UPDATE " + this.sqlAccess.GetTheDBName() + ".questitems " + 
+					"SET " + 
+            		"item_quantity = ? " + 
+            		"WHERE quest_id = ? AND item_id = ?";
+			 PreparedStatement preparedStmt = conn.prepareStatement(query);
+			  preparedStmt.setString(1, String.valueOf(itemQuantity));
+			  preparedStmt.setString(2, String.valueOf(questItem.GetQuestId()));
+			  preparedStmt.setString(3, String.valueOf(questItem.GetItemId()));
 		      preparedStmt.execute();
 		      success = true;
 		} catch (Exception e) {
